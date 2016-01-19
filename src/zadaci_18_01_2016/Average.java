e38ccaeab100cbe9f0a3eebfddb3d43b97475c08
Amilica <amila.galijasevic@gmail.com>
2016-01-19 09:29:21 +0100

    zadaci_18_01_2016

--- src/zadaci_18_01_2016/Average.java
+++ src/zadaci_18_01_2016/Average.java
@@ -9,14 +9,14 @@ public class Average {
 		java.util.Scanner input = new java.util.Scanner(System.in);
 		// list for storing numbers
 		ArrayList<Double> list = new ArrayList<>(100);
-		System.out.println("Enter number, 0 breaks the string");	
+		System.out.println("Enter number, 0 breaks the string");
 		double num = input.nextInt();
 		double sum = 0;
 		double count = 0;
 		double average = 0;
 		int countplus = 0;// counts bigger numbers than average
 		int countminus = 0;// count smaller numbers
-		
+
 		// stores the numbers to list while input isn't 0
 		while (num != 0) {
 			list.add(num);
@@ -24,6 +24,10 @@ public class Average {
 			sum += num;
 			// counts them
 			count++;
+			// breaks after counter hits 100
+			if (count == 100) {
+				break;
+			}
 			num = input.nextInt();
 		}
 		// calculates average
@@ -42,7 +46,7 @@ public class Average {
 		System.out.println("Number of bigger or equal numbers to average in list is: " + countplus);
 		System.out.println("Number of smaller numbers than average in list is: " + countminus);
 		try {
-			
+
 		} catch (InputMismatchException ey) {
 			System.out.println("Wrong input");
 		}
