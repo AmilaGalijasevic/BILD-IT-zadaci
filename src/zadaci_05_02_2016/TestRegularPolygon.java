10a5fa2d9b054b300d671365f4cf973573d52ed3
Amilica <amila.galijasevic@gmail.com>
2016-02-06 22:55:06 +0100

    zadaci_05_02_2016

--- src/zadaci_05_02_2016/TestRegularPolygon.java
+++ src/zadaci_05_02_2016/TestRegularPolygon.java
@@ -8,6 +8,7 @@ public class TestRegularPolygon {
 		RegularPolygon rp2 = new RegularPolygon(6, 4);
 		RegularPolygon rp3 = new RegularPolygon(10, 4, 5.6, 7.8);
 		// displays their perimetar and area depending on the inputed values
+		
 		System.out.println("Perimetar: " + rp1.getPerimetar() + " Area: " + rp1.getArea());
 		System.out.println("Perimetar: " + rp2.getPerimetar() + " Area: " + rp2.getArea());
 		System.out.println("Perimetar: " + rp3.getPerimetar() + " Area: " + rp3.getArea());
