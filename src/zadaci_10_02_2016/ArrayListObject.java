a34920422e789837cf1002001a82ed71cbebe042
Amilica <amila.galijasevic@gmail.com>
2016-02-11 17:52:57 +0100

    jos jedna :D

--- src/zadaci_10_02_2016/ArrayListObject.java
+++ src/zadaci_10_02_2016/ArrayListObject.java
@@ -8,34 +8,23 @@ public class ArrayListObject {
 	public static void main(String[] args) {
 		// nre array list of objects
 		ArrayList<Object> list = new ArrayList<>();
-		// array list of circle objects
-		ArrayList<Circle> ce = new ArrayList<>();
 		// creates new circle
 		Circle c = new Circle();
-		// adds that circle to list of circles
-		ce.add(c);
-		// adds the list of circles to the list of objects
-		list.addAll(ce);
-		// date list
-		ArrayList<Date> d = new ArrayList<>();
+		// adds the circle to the list of objects
+		list.add(c);
 		// new date
 		Date date = new Date();
-		// adds the date to the list
-		d.add(date);
-		// adds the list of dates to the list
-		list.addAll(d);
+		// adds date to the list
+		list.add(date);
 		// new string
 		String s = "Neki string";
 		// adds the string to the list
 		list.add(s);
 		// since i don't have a loan class and don't know what was supposed to
-		// be in it
-		ArrayList<Triangle> triangle = new ArrayList<>();
-		//i'm using triangle :)
+		// be in it i'm using triangle :)
 		Triangle t = new Triangle();
-		//adds it to the list
-		triangle.add(t);
-		list.addAll(triangle);
+		// adds it to the list
+		list.add(t);
 
 		// prints the objects in the list
 		for (int i = 0; i < list.size(); i++) {
